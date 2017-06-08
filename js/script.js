b6924f8409cbb66b59e8aebfd4f970d8909162d1
JeffryantoW <jeffryanto.wijaya@gmail.com>
2017-06-08 17:09:52 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,78 @@
+var header;
+
 $(document).ready(function(){
-var header = window.location.hash;
-	console.log(header);
+	header = window.location.hash;
 	if (header == '#biscuit') {
-		
+		$('#biscuit-wrapper').css({
+			'display': 'inline',
+		});
+		$('#crackers-wrapper').css({
+			'display': 'none',
+		});
+		$('#chips-wrapper').css({
+			'display': 'none',
+		});
 	}
-	if (header == '#crackers'){
+	if (header == '#crackers') {
+		$('#crackers-wrapper').css({
+			'display': 'inline',
+		});
+		$('#biscuit-wrapper').css({
+			'display': 'none',
+		});
+		$('#chips-wrapper').css({
+			'display': 'none',
+		});
+	}
+		if (header == '#chips') {
+		$('#crackers-wrapper').css({
+			'display': 'none',
+		});
+		$('#biscuit-wrapper').css({
+			'display': 'none',
+		});
+		$('#chips-wrapper').css({
+			'display': 'inline',
+		});
 	}
 });
+function openSnacks(param){
 
+	header = window.location.hash;
+	console.log(header);
+	if (param == 'biscuit') {
+		$('#biscuit-wrapper').css({
+			'display': 'inline',
+		});
+		$('#crackers-wrapper').css({
+			'display': 'none',
+		});
+		$('#chips-wrapper').css({
+			'display': 'none',
+		});
+	}
+	if (param == 'crackers'){
+		$('#crackers-wrapper').css({
+			'display': 'inline',
+		});
+		$('#biscuit-wrapper').css({
+			'display': 'none',
+		});
+		$('#chips-wrapper').css({
+			'display': 'none',
+		});
+	}
+
+		if (param == 'chips'){
+		$('#crackers-wrapper').css({
+			'display': 'none',
+		});
+		$('#biscuit-wrapper').css({
+			'display': 'none',
+		});
+		$('#chips-wrapper').css({
+			'display': 'inline',
+		});
+	}
+
+}
